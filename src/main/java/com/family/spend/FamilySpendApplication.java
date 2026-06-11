a50d19e965b41c1345998ed8ea205eaec3c294bd
hien <phamthehienc20042000@gmail.com>
2026-06-11 13:52:48 +0700

    init

--- src/main/java/com/family/spend/FamilySpendApplication.java
+++ src/main/java/com/family/spend/FamilySpendApplication.java
@@ -2,10 +2,8 @@ package com.family.spend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class FamilySpendApplication {
     public static void main(String[] args) {
         SpringApplication.run(FamilySpendApplication.class, args);
